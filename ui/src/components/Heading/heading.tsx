ec3254645a8f467ee218ebc4b514ae1d24750d3e
Edgar Muniz Berlinck <edgar.berlinck@ingka.com>
2023-02-03 00:28:03 +0100

    # UI
     - Adding zustand
     - Creating the store
     - Styling the Heading

--- ui/src/components/Heading/heading.tsx
+++ ui/src/components/Heading/heading.tsx
@@ -1,5 +1,28 @@
+import { usePageStore } from "@/store/pagesStore"
+import { Inter } from '@next/font/google'
+
+const brandFont = Inter({ weight: "400", subsets: ['latin'] })
+
 export default function Heading () {
-  return <div>
-    Heading
+  const pages = usePageStore(store => store.pages)
+
+  return <div className="relative bg-slate-200 dark:bg-slate-900">
+    <div className="max-w-7xl px-1">
+      <div className="flex items-center py-5 px-2 justify-start sm:space-x-5 text-black dark:text-white">
+        <div className="flex justify-start lg:w-0 lg:flex-1">
+          <a href="/">
+            {/* TODO: Should come from Strapi */}
+            <span className="sr-only">edgarberlinck.com</span>
+            <span className={`${brandFont.className} md:text-2xl sm:text-xl hover:underline`}>edgarberlinck.com</span>
+          </a>
+        </div>
+        
+        { pages?.map((page) => (
+          <span key={page.id} className="text-base font-medium text-gray-500 hover:text-gray-900">
+            {page.attributes.title}
+          </span>
+        )) }
+      </div>
+    </div>
   </div>
 }
