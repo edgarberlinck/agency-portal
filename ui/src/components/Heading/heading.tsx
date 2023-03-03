2ebb92426a8c893ad32b4e85cca30012bbdd1f27
Edgar Muniz Berlinck <edgar.berlinck@ingka.com>
2023-03-03 16:46:51 +0100

    - Strapi Upgrade - Styling Hero - Using next/image to render images

--- ui/src/components/Heading/heading.tsx
+++ ui/src/components/Heading/heading.tsx
@@ -6,23 +6,25 @@ const brandFont = Kanit({ weight: "400", subsets: ['latin'] })
 
 export default function Heading () {
   const pages = usePageStore(store => store.pages)
+  console.log(pages)
 
   return <div className="relative bg-slate-200 dark:bg-slate-900">
     <div className="max-w-7xl px-1">
-      <div className="flex items-center py-5 px-2 justify-start sm:space-x-5 text-black dark:text-white">
-        <div className="flex justify-start lg:w-0 lg:flex-1">
+      <div className="flex items-center py-5 px-2 justify-start align-middle space-x-5 text-black dark:text-white">
+        <div className="flex justify-start">
           <Link href='/'>
             {/* TODO: Should come from Strapi */}
             <span className="sr-only">edgarberlinck.com</span>
             <span className={`${brandFont.className} md:text-2xl sm:text-xl hover:underline`}>edgarberlinck.com</span>
           </Link>
         </div>
-        
-        { pages?.map((page) => (
-          <span key={page.id} className="text-base font-medium text-gray-500 hover:text-gray-900">
-            {page.attributes.title}
-          </span>
-        )) }
+        <div className='flex px-8 gap-4 w-full'>
+          { pages?.map((page) => (
+            <span key={page.id} className="text-base font-medium text-gray-500 hover:text-gray-900">
+              {page.attributes.title}
+            </span>
+          )) }
+        </div>
       </div>
     </div>
   </div>
--- ui/src/components/Hero/hero.tsx
+++ ui/src/components/Hero/hero.tsx
@@ -1,4 +1,5 @@
 import { StrapiImage } from "@/common/types/strapi"
+import Image from 'next/image'
 interface Props {
   description: string
   title: string
@@ -6,9 +7,21 @@ interface Props {
 }
 
 export default function Hero ({ description, title, image }: Props) {
-  return <div>
-    <h1>{title}</h1>
-    <p>{description}</p>
-    {image && <img src={`${process.env.NEXT_PUBLIC_MEDIA_PATH}${image.data?.attributes.formats.large?.url}`} />}
+  return <div className='p-4 m-0 flex gap-4'>
+    {image && (
+      <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter">
+        <a href="#">
+          <Image className='h-auto max-w-full rounded-lg shadow-md shadow-gray-700' src={`${process.env.NEXT_PUBLIC_MEDIA_PATH}${image.data?.attributes.formats.large?.url}`} alt={description} width={500} height={500}/>
+        </a>
+        <figcaption className="absolute text-center px-4 text-lg text-white bottom-6">
+          <p>Give-me this candy. Pleaaaaaaaase</p>
+        </figcaption>
+      </figure>
+    )}
+    <div>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{title}</h1>
+      <hr></hr>
+      <p className="mb-6 mt-4 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">{description}</p>
+    </div>
   </div>
 }
